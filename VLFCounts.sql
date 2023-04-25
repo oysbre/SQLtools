869ac44ba12a76ea4b69988c56ef55393c1d65cf
oysbre <95480044+oysbre@users.noreply.github.com>
2023-04-25 12:35:04 +0200

    Update VLFCounts.sql

--- VLFCounts.sql
+++ VLFCounts.sql
@@ -1,5 +1,10 @@
-/* Get VLF Counts for all databases on the instance */
-/* Ff above 100, shrink LOG file and set Autogrowth higher in MB not % */
+/* Get VLF Counts for all databases on the instance 
+Ff above 100, shrink LOG file and set Autogrowth higher in MB not % 
+When VLF under 100 – ignore, between 100 – 200 – you can ignore, but better to fix
+When above 400 – it’s getting urgent, so fix it.
+When above 600 – slowdowns are happening, but it’s not easy to diagnose these. Fix.
+When above 5000, fix now!
+*/
 CREATE TABLE #VLFInfo (Recoveryunitid int ,FileID  int,
 					   FileSize bigint, StartOffset bigint,
 					   FSeqNo      bigint, [Status]    bigint,
