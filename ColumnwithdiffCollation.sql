00e5ee2934c45ec4209b18c64c7acaf6a43c6ccd
oysbre <95480044+oysbre@users.noreply.github.com>
2023-11-15 10:35:17 +0100

    Update ColumnwithdiffCollation.sql

--- ColumnwithdiffCollation.sql
+++ ColumnwithdiffCollation.sql
@@ -11,5 +11,5 @@ INNER JOIN
 INNER JOIN 
     sys.types ty ON c.system_type_id = ty.system_type_id    
 WHERE 
-    t.is_ms_shipped = 0 AND ty.name <> 'sysname'   AND  c.collation_name <> 'SQL_Latin1_General_CP1_CI_AS')
+    t.is_ms_shipped = 0 AND ty.name <> 'sysname'   AND  c.collation_name <> 'SQL_Latin1_General_CP1_CI_AS'
 	
