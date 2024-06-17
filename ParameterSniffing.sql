e678c53f4338cd100c10b992676f3b05a24990a5
oysbre <95480044+oysbre@users.noreply.github.com>
2024-06-17 08:27:00 +0200

    Update ParameterSniffing.sql

--- ParameterSniffing.sql
+++ ParameterSniffing.sql
@@ -8,7 +8,10 @@ SUBSTRING(ST.text, (QS.statement_start_offset / 2) + 1, ((CASE statement_end_off
                                                     END - QS.statement_start_offset) / 2) + 1) AS [Query Statement], 
         ST.text AS 'Procedure Batch',
         qs.execution_count,
-        min_worker_time, max_worker_time,
+        qp.query_plan,
+	    TRY_CONVERT(XML,SUBSTRING(etqp.query_plan,CHARINDEX('<ParameterList>',etqp.query_plan), CHARINDEX('</ParameterList>',etqp.query_plan) + LEN('</ParameterList>') - CHARINDEX('<ParameterList>',etqp.query_plan) )) AS Parameters,
+		min_worker_time,
+		max_worker_time,
         ISNULL((max_worker_time - min_worker_time) / NULLIF(min_worker_time, 0), 0) AS LogicalCpuRatio, 
         min_logical_reads,max_logical_reads,
         ISNULL((max_logical_reads - min_logical_reads) / NULLIF(min_logical_reads, 0), 0) AS LogicalReadsDevRatio,
@@ -17,9 +20,11 @@ SUBSTRING(ST.text, (QS.statement_start_offset / 2) + 1, ((CASE statement_end_off
 FROM sys.dm_exec_query_stats AS QS
 CROSS APPLY sys.dm_exec_sql_text(QS.sql_handle) AS ST
 CROSS APPLY sys.dm_exec_plan_attributes(qs.plan_handle) qpa
+CROSS APPLY sys.dm_exec_text_query_plan(qs.plan_handle, qs.statement_start_offset, qs.statement_end_offset) etqp
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
 WHERE attribute = 'dbid' 
 )
 		
         SELECT * FROM Execution_Detail WHERE LogicalCpuRatio >=100 AND LogicalReadsDevRatio>=100 AND LogicalElapsedTimDevRatio>=100 AND [Query Statement] like 'SELECT%'
-		--order by LogicalElapsedTimDevRatio desc
-		order by LogicalReadsDevRatio desc
+		order by LogicalElapsedTimDevRatio desc
+		--order by LogicalReadsDevRatio desc
