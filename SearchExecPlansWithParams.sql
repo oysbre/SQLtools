5474b96c0d959623413644f567408d933cf88650
oysbre <95480044+oysbre@users.noreply.github.com>
2024-07-30 14:04:43 +0200

    Update SearchExecPlansWithParams.sql

--- SearchExecPlansWithParams.sql
+++ SearchExecPlansWithParams.sql
@@ -8,27 +8,29 @@ WHEN -1 THEN DATALENGTH(qt.TEXT)
 ELSE qs.statement_end_offset
 END - qs.statement_start_offset)/2)+1) AS sqlquery,
 qs.execution_count,
-qs.total_logical_reads, qs.last_logical_reads,
-qs.total_logical_writes, qs.last_logical_writes,
-qs.total_worker_time,
-qs.last_worker_time
+qs.min_logical_reads, qs.max_logical_reads,qs.last_logical_reads,
+qs.min_rows,qs.max_rows,
+qs.total_worker_time,qs.last_worker_time
 ,CAST(qs.total_elapsed_time / 1000000.0 AS DECIMAL(28, 2)) AS [Total Duration (s)]
 ,CAST (qs.last_elapsed_time / 1000000.0 AS DECIMAL(28, 2)) AS [Last_elapsed_(s)]
 ,CAST(qs.total_worker_time * 100.0 / qs.total_elapsed_time AS DECIMAL(28, 2)) AS [% CPU]
-,qs.last_execution_time, qp.query_plan,
-REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(CAST(CONVERT(XML,SUBSTRING(etqp.query_plan,CHARINDEX('<ParameterList>',etqp.query_plan), CHARINDEX('</ParameterList>',etqp.query_plan) + LEN('</ParameterList>') - CHARINDEX('<ParameterList>',etqp.query_plan) )) AS NVARCHAR(MAX)),'<',''),'>',''),'"',''),'/',' '),'ParameterList',''),'ColumnReference Column=',''),'ParameterDataType=',''),'ParameterCompiledValue=','')  AS CompiledParameters 
+,qs.last_execution_time
+,qp.query_plan
+/* REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(CAST(CONVERT(XML,SUBSTRING(etqp.query_plan,CHARINDEX('<ParameterList>',etqp.query_plan), CHARINDEX('</ParameterList>',etqp.query_plan) + LEN('</ParameterList>') - CHARINDEX('<ParameterList>',etqp.query_plan) )) AS NVARCHAR(MAX)),'<',''),'>',''),'"',''),'/',' '),'ParameterList',''),'ColumnReference Column=',''),'ParameterDataType=',''),'ParameterCompiledValue=','')  AS CompiledParameters */
 
 FROM sys.dm_exec_query_stats qs
 CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
 CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
 CROSS APPLY sys.dm_exec_text_query_plan(qs.plan_handle, qs.statement_start_offset, qs.statement_end_offset) etqp
 
-where qt.text like '%SELECT%' --COLLATE SQL_Latin1_General_CP1_CS_AS
+where qt.text like '%SELECT%' COLLATE SQL_Latin1_General_CP1_CS_AS
+and qt.text like '%INVENTDIM%' 
+--and qt.text like '%INVENTLOCATIONID%'
+--and qp.dbid = 9
 --and qt.text like '%fast 1%'
-and qt.text like '%INVENTDIM%'
 --and last_logical_reads > 100000
 --and qs.last_elapsed_time/1000000 > 400
---and execution_count >  3
+and execution_count >  1
 --ORDER BY qs.total_logical_reads DESC -- logical reads
 -- ORDER BY qs.total_logical_writes DESC -- logical writes
 --ORDER BY execution_count DESC -- execution count
