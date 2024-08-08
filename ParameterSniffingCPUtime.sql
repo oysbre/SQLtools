f7745d1ffa0a8bfcf44887003038c602e1b5628d
oysbre <95480044+oysbre@users.noreply.github.com>
2024-08-08 12:52:26 +0200

    Update ParameterSniffingCPUtime.sql

--- ParameterSniffingCPUtime.sql
+++ ParameterSniffingCPUtime.sql
@@ -1,10 +1,9 @@
 /* Find queries with parameter sniffing issues, look for ones with much different CPU usage. */
-SELECT TOP (25)  DB_NAME(dp.dbid) AS [Database Name],
-ds.execution_count,
-       ds.min_worker_time / 1000000. AS min_worker_time_ms,
-       ds.max_worker_time / 1000000. AS max_worker_time_ms,
-       ds.min_elapsed_time / 1000000. AS min_elapsed_time_ms,
-       ds.max_elapsed_time / 1000000. AS max_elapsed_time_ms,
+SELECT TOP (10) ds.execution_count,
+       ds.min_worker_time / 1000. AS min_worker_time_ms,
+       ds.max_worker_time / 1000. AS max_worker_time_ms,
+       ds.min_elapsed_time / 1000. AS min_elapsed_time_ms,
+       ds.max_elapsed_time / 1000. AS max_elapsed_time_ms,
        ds.min_logical_reads,
        ds.max_logical_reads,
        ds.min_rows,
@@ -20,9 +19,9 @@ FROM sys.dm_exec_query_stats AS ds
 CROSS APPLY sys.dm_exec_sql_text(ds.plan_handle) AS st
 CROSS APPLY sys.dm_exec_query_plan(ds.plan_handle) AS dp
 WHERE st.dbid = DB_ID()
-AND st.text like '%SELECT%'
-
 AND ds.execution_count > 1
 AND (ds.min_worker_time / 1000000.) * 100. < (ds.max_worker_time / 1000000.)
+--AND st.text like '%INVENTDIM%'
+--AND st.text like '%FAST%'
 ORDER BY max_worker_time_ms DESC
 OPTION(RECOMPILE);
