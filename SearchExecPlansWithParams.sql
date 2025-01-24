80c5318dfffb3e662c9549628ec3e6c9c87367d9
oysbre <95480044+oysbre@users.noreply.github.com>
2025-01-24 12:56:52 +0100

    Update SearchExecPlansWithParams.sql

--- SearchExecPlansWithParams.sql
+++ SearchExecPlansWithParams.sql
@@ -16,7 +16,7 @@ qs.total_worker_time,qs.last_worker_time
 ,CAST(qs.total_worker_time * 100.0 / qs.total_elapsed_time AS DECIMAL(28, 2)) AS [% CPU]
 ,qs.last_execution_time
 ,qp.query_plan
-/* REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(CAST(CONVERT(XML,SUBSTRING(etqp.query_plan,CHARINDEX('<ParameterList>',etqp.query_plan), CHARINDEX('</ParameterList>',etqp.query_plan) + LEN('</ParameterList>') - CHARINDEX('<ParameterList>',etqp.query_plan) )) AS NVARCHAR(MAX)),'<',''),'>',''),'"',''),'/',' '),'ParameterList',''),'ColumnReference Column=',''),'ParameterDataType=',''),'ParameterCompiledValue=','')  AS CompiledParameters */
+/* REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(CAST(TRY_CONVERT(XML,SUBSTRING(etqp.query_plan,CHARINDEX('<ParameterList>',etqp.query_plan), CHARINDEX('</ParameterList>',etqp.query_plan) + LEN('</ParameterList>') - CHARINDEX('<ParameterList>',etqp.query_plan) )) AS NVARCHAR(MAX)),'<',''),'>',''),'"',''),'/',' '),'ParameterList',''),'ColumnReference Column=',''),'ParameterDataType=',''),'ParameterCompiledValue=','')  AS CompiledParameters */
 
 FROM sys.dm_exec_query_stats qs
 CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
@@ -24,13 +24,12 @@ CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
 CROSS APPLY sys.dm_exec_text_query_plan(qs.plan_handle, qs.statement_start_offset, qs.statement_end_offset) etqp
 
 where qt.text like '%SELECT%' COLLATE SQL_Latin1_General_CP1_CS_AS
-and qt.text like '%INVENTDIM%' 
---and qt.text like '%INVENTLOCATIONID%'
+-- and qt.text like '%INVENTDIM%' 
 --and qp.dbid = 9
---and qt.text like '%fast 1%'
+--and qt.text like '%FAST%'
 --and last_logical_reads > 100000
 --and qs.last_elapsed_time/1000000 > 400
-and execution_count >  1
+and execution_count >  1 
 --ORDER BY qs.total_logical_reads DESC -- logical reads
 -- ORDER BY qs.total_logical_writes DESC -- logical writes
 --ORDER BY execution_count DESC -- execution count
